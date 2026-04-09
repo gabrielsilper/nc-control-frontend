1c39d400bb7f5308c0521ef876647ba5f60165d4
gabrielsilper <gabrielsilper.main@gmail.com>
2026-04-08 20:12:20 -0400

    feat: criação das páginas de home e not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { Home } from './pages/home/home';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: 'home', component: Home },
+  { path: '**', component: NotFound },
+];
