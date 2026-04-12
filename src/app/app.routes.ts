eea1c300339c5f0bd4d47cb89b398b25ddc9a647
gabrielsilper <gabrielsilper.main@gmail.com>
2026-04-12 12:02:41 -0400

    feat: cria tela de dashboard e remove tela home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
-import { Home } from './pages/home/home';
 import { NotFound } from './pages/not-found/not-found';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'login', component: Login },
+  { path: 'dashboard', component: Dashboard },
   { path: 'register', component: Register },
-  { path: 'home', component: Home },
   { path: '**', component: NotFound },
 ];
