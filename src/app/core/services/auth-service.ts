82ea5165bcea42b02fdbe44b42596998a9b45f79
gabrielsilper <gabrielsilper.main@gmail.com>
2026-04-12 13:13:53 -0400

    feat: Implementa chamada de login ao projeto backend

--- src/app/core/services/auth-service.ts
+++ src/app/core/services/auth-service.ts
@@ -1,15 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { AuthResponse, LoginRequest } from '../models/auth.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private readonly http = inject(HttpClient);
-  private readonly API_URL = 'https://sua-api.com/auth/login';
+  private readonly API_URL = `${environment.apiUrl}/auth`;
 
   login(credentials: LoginRequest) {
-    return this.http.post<AuthResponse>(this.API_URL, credentials);
+    return this.http.post<AuthResponse>(`${this.API_URL}/login`, credentials);
   }
 }
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../core/services/auth-service';
+import { Router } from '@angular/router';
+import { LoginRequest } from '../../core/models/auth.model';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      const credentials = this.loginForm.value as LoginRequest;
+
+      this.authService.login(credentials).subscribe({
+        next: (response) => {
+          console.log(response)
+          this.router.navigate(['/dashboard']);
+        },
+        error: (err) => {
+          console.error('Erro no componente de login', err);
+        },
+      });
+    }
+  }
+}
