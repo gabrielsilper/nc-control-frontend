0b40afffb7ac243273af4233699fe53b6e13bb8e
gabrielsilper <gabrielsilper.main@gmail.com>
2026-04-14 22:35:01 -0400

    wip: inicia implemntação da página de dashboard

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { InfoCard } from "../../shared/components/info-card/info-card";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [InfoCard],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
---
+++ src/app/shared/components/info-card/info-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'info-card',
+  imports: [],
+  templateUrl: './info-card.html',
+  styleUrl: './info-card.css',
+})
+export class InfoCard {}
