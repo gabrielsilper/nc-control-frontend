37dcd563595bf856b477253690978c7f95a1c11a
gabrielsilper <gabrielsilper.main@gmail.com>
2026-04-15 20:26:48 -0400

    feat: adiciona o component de badge

---
+++ src/app/core/models/non-conformity.model.ts
@@ -0,0 +1,6 @@
+export enum SeverityNc {
+  BAIXA,
+  MEDIA,
+  ALTA,
+  CRITICA,
+}
--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { InfoCard } from "../../shared/components/info-card/info-card";
+import { Bagde } from "../../shared/components/bagde/bagde";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [InfoCard],
+  imports: [InfoCard, Bagde],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
