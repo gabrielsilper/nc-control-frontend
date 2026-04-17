595b670e24d1deabc2a2ce06fde2e43a4a92128c
gabrielsilper <gabrielsilper.main@gmail.com>
2026-04-16 23:22:41 -0400

    feat: pequena melhoria do info-card do dashboard

--- src/app/shared/components/info-card/info-card.ts
+++ src/app/shared/components/info-card/info-card.ts
@@ -11,5 +11,20 @@ export class InfoCard {
   @Input() label: string = '';
   @Input() value: number | string = '';
   @Input() icon: string = '';
-  @Input() type: 'success' | 'danger' | 'warning' | 'neutral' = 'neutral';
+  @Input() type: 'success' | 'alert' | 'warn' | 'medium' | 'common' = 'common';
+
+  get infoCardBorderClass() {
+    switch (this.type) {
+      case 'success':
+        return 'info-card-succes-border';
+      case 'alert':
+        return 'info-card-alert-border';
+      case 'warn':
+        return 'info-card-warn-border';
+      case 'medium':
+        return 'info-card-medium-border';
+      default:
+        return 'info-card-common-border';
+    }
+  }
 }
