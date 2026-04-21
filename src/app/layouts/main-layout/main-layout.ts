e7d9392feba40e81b30053fbafaaccf6ff9847d9
gabrielsilper <gabrielsilper.main@gmail.com>
2026-04-20 21:25:49 -0400

    wip: inicia sidebar menu

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarMenu } from "../../shared/components/sidebar-menu/sidebar-menu";
 
 @Component({
   selector: 'main-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarMenu],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css',
 })
---
+++ src/app/shared/components/sidebar-menu/sidebar-menu.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sidebar-menu',
+  imports: [],
+  templateUrl: './sidebar-menu.html',
+  styleUrl: './sidebar-menu.css',
+})
+export class SidebarMenu {}
